fe230785fa72bc3611688f517fee848d6b386ec4
jackathang <127996135+jackathang@users.noreply.github.com>
2025-11-17 16:37:47 -0600

    Set up logging middleware

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
 import express, { NextFunction, Request, Response } from 'express';
 import notesRouter from './routes/notes';
+import { logger } from './middleware/logger';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
+// middlewares
+app.use(logger);
+
 // API routes
 app.use('/api/v1/notes', notesRouter);
 
 // Server setup
-// const PORT = process.env.PORT;
-
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
 });
